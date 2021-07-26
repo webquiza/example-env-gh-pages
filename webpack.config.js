b7f6003853910b64b2fa9e3d43be9819c0f68c3e
Carlos Urquiza <webquiza@gmail.com>
2021-07-25 20:01:26 -0700

    add gh-pages functionality

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: "/example-env-gh-pages/"
   },
   devtool: 'eval-source-map',  
   devServer: {                 
